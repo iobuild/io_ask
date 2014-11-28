806c7185e02d267f095965d7086527d8569ceb64
mykingla <kingla_pei@163.com>
2014-11-28 09:48:00 +0800

    add comments controller

---
+++ app/controllers/io_ask/comments_controller.rb
@@ -0,0 +1,48 @@
+module IoAsk
+
+  class CommentsController < IoAsk::ApplicationController
+
+    before_filter :pre_load
+
+    def pre_load
+      @topic = IoAsk::Topic.find(params[:topic_id]) if params[:topic_id]
+      @comment = IoAsk::Comment.find(params[:id]) if params[:id]
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content, :topic_id, :user_id)
+    end
+
+
+    def create
+      @comments = @topic.comments
+      @comment = @comments.build(comment_params)
+      @comment.user = current_user
+
+      if @comment.save
+        anchor = '#comment-' + @comment.id.to_s
+        return redirect_to "/ask/topics/#{@topic.id}?page=#{@comments.page.total_pages}" + anchor      
+      end
+
+      redirect_to :back
+    end
+
+
+    def edit
+    end
+
+    def update
+      return redirect_to "/ask/topics/#{@comment.commentable.id}" if @comment.update_attributes(comment_params)
+
+      render 'edit'
+
+    end
+
+
+    def destroy
+      return redirect_to "/ask/topics/#{@comment.commentable.id}" if @comment.destroy
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 IoAsk::Engine.routes.draw do
   resources :topics
+
+  resources :topics do
+    resources :comments, :shallow => true
+  end
 end
