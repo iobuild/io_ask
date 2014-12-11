8b371d245e447e5c8633147e65808e330727a8a7
mykingla <kingla_pei@163.com>
2014-12-11 19:05:14 +0800

    fix ioask_avatar

--- app/helpers/io_ask/topics_helper.rb
+++ app/helpers/io_ask/topics_helper.rb
@@ -2,6 +2,7 @@ module IoAsk
   module TopicsHelper
     def ioask_avatar(user, options = {})
       image = user.ioask_avatar
+      image = image.gsub(/\/assets\//, '')
 
       image_tag image, :alt => "#{user.ioask_name}" if image.present?
     end
